47f8da01ec93ae51c1aa8e0741b645024561e67d
Demmy Dwi Rhamadan <demmy.up@gmail.com>
2019-12-05 13:36:33 +0700

    adding getIngredient and state to provider

--- lib/provider/ingredient_provider.dart
+++ lib/provider/ingredient_provider.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tech_task/model/ingredient.dart';
+import 'package:tech_task/network/api_manager.dart';
 
 class IngredientProvider with ChangeNotifier {
-
   List<Ingredient> _ingredients;
 
-  List<Ingredient> get ingredients  => _ingredients;
+  List<Ingredient> get ingredients => _ingredients;
 
   set ingredients(List<Ingredient> newValue) {
     this._ingredients = newValue;
     notifyListeners();
   }
 
+  IngredientState _state = IngredientState.onInit;
+
+  IngredientState get state => _state;
+
+  set state(IngredientState newValue) {
+    this._state = newValue;
+    notifyListeners();
+  }
+
+  String _errMessage;
+
+  String get errMessage => _errMessage;
+
+  set errMessage(String newValue) {
+    this._errMessage = newValue;
+    this._state = IngredientState.onError;
+    notifyListeners();
+  }
+
+  refresh() {
+    _state = IngredientState.onInit;
+    _ingredients = [];
+    notifyListeners();
+  }
+
+  getIngredients() {
+    ApiManager.getIngredients(onError: (errMessage) {
+      this.errMessage = errMessage;
+    }, onSuccess: (data) {
+      _ingredients =
+          data.map((item) => Ingredient.fromJson(item.toJson())).toList();
+      if (_ingredients.isEmpty) {
+        _state = IngredientState.onEmpty;
+      } else {
+        _state = IngredientState.onLoaded;
+      }
+      print(data.length);
+      notifyListeners();
+    });
+  }
 }
 
-enum IngredientState {
-  onRefresh, onLoading, onLoaded, onEmpty, onError
-}
+enum IngredientState { onInit, onLoaded, onEmpty, onError }
