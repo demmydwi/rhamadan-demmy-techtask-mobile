501420a1db92d5d132bcfcdd90b594f8f5b54a0a
Demmy Dwi Rhamadan <demmy.up@gmail.com>
2019-12-05 13:36:01 +0700

    adding multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tech_task/component/color.dart';
+import 'package:tech_task/provider/ingredient_provider.dart';
 import 'package:tech_task/screen/launcher/launch_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MultiProvider(
+      child: MyApp(),
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => IngredientProvider(),
+        ),
+      ],
+    ));
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,8 +25,10 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.white,
         accentColor: MyColor.secondary,
         accentColorBrightness: Brightness.light,
-        primaryColorBrightness: Brightness.light,
-        primaryIconTheme: IconThemeData(color: Colors.white, ),
+        primaryColorBrightness: Brightness.dark,
+        primaryIconTheme: IconThemeData(
+          color: Colors.white,
+        ),
         primaryTextTheme: TextTheme(),
         accentTextTheme: TextTheme(),
       ),
