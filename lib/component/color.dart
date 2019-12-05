e255ec2fd126e18cf502310dec509595f5547636
Demmy Dwi Rhamadan <demmy.up@gmail.com>
2019-12-05 10:49:31 +0700

    adding color component

---
+++ lib/component/color.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static final primaryLight = Color(0xff579eca);
+  static final primary = Color(0xff1A7099);
+  static final primaryDark = Color(0xff00456b);
+
+  static final secondaryLight = Color(0xffcf6f44);
+  static final secondary = Color(0xff99421a);
+  static final secondaryDark = Color(0xff651500);
+
+  static final textColors = Colors.white;
+
+}
