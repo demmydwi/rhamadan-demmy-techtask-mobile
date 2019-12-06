1502f869e12c3baa73834bb2006ec3a352c42fcd
Demmy Dwi Rhamadan <demmy.up@gmail.com>
2019-12-06 23:10:39 +0700

    add empty view & error view on recipes page

--- lib/screen/recipe/recipe_page.dart
+++ lib/screen/recipe/recipe_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tech_task/provider/recipe_provider.dart';
+import 'package:tech_task/provider/state.dart';
 import 'package:tech_task/screen/recipe/recipe_item_view.dart';
 
 class RecipePage extends StatefulWidget {
@@ -16,6 +17,21 @@ class _RecipePageState extends State<RecipePage> {
           title: Text("Recipes"),
         ),
         body: Consumer<RecipeProvider>(builder: (_, _provider, __) {
+          if (_provider.state == ProviderState.onEmpty) {
+            return Center(
+              child: Text(
+                "Recipes not found",
+                style: Theme.of(context).textTheme.title,
+              ),
+            );
+          } else if (_provider.state == ProviderState.onError) {
+            return Center(
+              child: Text(
+                _provider.errMessage,
+                style: Theme.of(context).textTheme.title,
+              ),
+            );
+          }
           return ListView(
             padding: EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 16),
             children: _provider.recipes
