d7278860e289a5eb8fcc6717c96c1c4ac7d82109
Demmy Dwi Rhamadan <demmy.up@gmail.com>
2019-12-10 20:20:40 +0700

    add widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,22 +5,97 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tech_task/main.dart';
+import 'package:intl/intl.dart' as intl;
+import 'package:provider/provider.dart';
+import 'package:tech_task/function/date.dart';
+import 'package:tech_task/model/ingredient.dart';
+import 'package:tech_task/provider/ingredient_provider.dart';
+import 'package:tech_task/provider/recipe_provider.dart';
+import 'package:tech_task/screen/ingredient/ingredient_page.dart';
+import 'package:tech_task/screen/launcher/launch_page.dart';
+import 'package:tech_task/screen/recipe/recipe_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets(
+      "Launch page has label 'My Recipes' & image with key `launcher_image`",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: LaunchPage(
+        ignorePush: true,
+      ),
+    ));
+    // find my recipes label
+    final imageLauncher = find.byKey(ValueKey("launcher_image"));
+    final titleLauncher = find.text('My Recipes');
+    expect(imageLauncher, findsOneWidget);
+    expect(titleLauncher, findsOneWidget);
   });
+
+  testWidgets(
+      "Ingredient page has header, appbar, body, content, button, title, datepicker",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => IngredientProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => RecipeProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        home: IngredientPage(),
+      ),
+    ));
+
+    final header = find.byKey(ValueKey("ingredient_header"));
+    final appBar = find.byKey(ValueKey("ingredient_app_bar"));
+    final body = find.byKey(ValueKey("ingredient_body"));
+    final content = find.byKey(ValueKey("ingredient_content"));
+    final button = find.byKey(ValueKey("ingredient_button"));
+    final title = find.text('Ingredients');
+
+    expect(header, findsOneWidget);
+    expect(appBar, findsOneWidget);
+    expect(body, findsOneWidget);
+    expect(content, findsOneWidget);
+    expect(button, findsOneWidget);
+    expect(title, findsOneWidget);
+
+    var dateTimeNowString = userFormat.format(DateTime.now());
+    final datePicker = find.text(dateTimeNowString);
+
+    expect(datePicker, findsOneWidget);
+  });
+
+  testWidgets(
+      "Recipe page has appbar, body, content, title",
+          (WidgetTester tester) async {
+        await tester.pumpWidget(MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => IngredientProvider(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => RecipeProvider(),
+            ),
+          ],
+          child: MaterialApp(
+            home: RecipePage(),
+          ),
+        ));
+
+        final appBar = find.byKey(ValueKey("recipe_app_bar"));
+        final body = find.byKey(ValueKey("recipe_body"));
+        final content = find.byKey(ValueKey("recipe_content"));
+        final title = find.text('Recipes');
+
+        expect(appBar, findsOneWidget);
+        expect(body, findsOneWidget);
+        expect(content, findsOneWidget);
+        expect(title, findsOneWidget);
+      });
 }
